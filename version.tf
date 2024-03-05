6edc49016b66412afc762c149bb1115980571cbe
Umar <dvop.omer@gmail.com>
2024-03-05 21:06:03 +0530

    updated

--- version.tf
+++ version.tf
@@ -2,14 +2,14 @@ terraform {
   required_version = "~> 1.0"
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "3.0.0"
-  }
-   azapi = {
+      source  = "hashicorp/azurerm"
+      version = "3.0.0"
+    }
+    azapi = {
       source  = "azure/azapi"
       version = "~>1.5"
     }
-     random = {
+    random = {
       source  = "hashicorp/random"
       version = "~>3.0"
     }
@@ -17,12 +17,12 @@ terraform {
       source  = "hashicorp/time"
       version = "0.9.1"
     }
-}
+  }
 }
 
 provider "azurerm" {
-    features {
-      
-    }
-  
+  features {
+
+  }
+
 }
